c3372032b518969200965b9aa8a970f908d2ab7c
surajklmn <surajbhattarai339@gmail.com>
2026-05-27 10:34:38 +0000

    added css and js

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,10 +7,21 @@ import (
 
 func main(){
 	mux := http.NewServeMux()
+
+
+
+	//Create a file server which servers files out of the ".ui/static" directory
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	//Use the mux.Handle fucntion to register the fileServer as the handler for all URL path that start with "/static/"
+	mux.Handle("GET /static/",http.StripPrefix("/static",fileServer))
+
+
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}",snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create/",snippetCreatePost)
+
 	
 	log.Print("starting server  on :4000")
 
