8f1786695cf1bbaafccab744de33b9987e94fed1
surajklmn <surajbhattarai339@gmail.com>
2026-06-01 12:52:24 +0000

    centralized error handling and isolate application routes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -12,10 +12,10 @@ type application struct{//application struct to hold system wide dependencies
 }
 
 func main(){
-	mux := http.NewServeMux()
 
 	addr := flag.String("addr",":4000","HTTP network address")
 	flag.Parse()
+
 	logger := slog.New(slog.NewTextHandler(os.Stdout,&slog.HandlerOptions{
 		AddSource :true,
 	}))
@@ -24,21 +24,10 @@ func main(){
 		logger: logger,
 	}
 
-	//Create a file server which servers files out of the ".ui/static" directory
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	//Use the mux.Handle fucntion to register the fileServer as the handler for all URL path that start with "/static/"
-	mux.Handle("GET /static/",http.StripPrefix("/static",fileServer))
-
-
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /snippet/view/{id}",app.snippetView)
-	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-	mux.HandleFunc("POST /snippet/create/",app.snippetCreatePost)
-
 	logger.Info("starting server",slog.String("addr", *addr))
-	
-	err := http.ListenAndServe(*addr,mux)
+
+	//Call the app.routes() method and pass that to ListenAndServe
+	err := http.ListenAndServe(*addr,app.routes())
 
 	logger.Error(err.Error())
 	os.Exit(1)
---
+++ cmd/web/routes.go
@@ -0,0 +1,20 @@
+package main 
+
+import "net/http"
+
+func (app *application) routes() *http.ServeMux{
+mux := http.NewServeMux()
+
+	//Create a file server which servers files out of the ".ui/static" directory
+fileServer := http.FileServer(http.Dir("./ui/static"))
+
+	//Use the mux.Handle fucntion to register the fileServer as the handler for all URL path that start with "/static/"
+mux.Handle("GET /static/", http.StripPrefix("/static",fileServer))
+
+mux.HandleFunc("GET /{$}",app.home)
+mux.HandleFunc("GET /snippet/view/{id}",app.snippetView)
+mux.HandleFunc("GET /snippet/create",app.snippetCreate)
+mux.HandleFunc("POST /snippet/create",app.snippetCreatePost)
+
+return mux
+}
