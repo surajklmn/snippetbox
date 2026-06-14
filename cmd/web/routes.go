ff0459a3e08f00a533f4c743147824072366625a
surajklmn <surajbhattarai339@gmail.com>
2026-06-14 15:28:02 +0000

    Composable middleware chains using alice

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,6 +1,9 @@
 package main 
 
-import "net/http"
+import (
+	"net/http"
+	"github.com/justinas/alice"
+)
 
 func (app *application) routes() http.Handler{
 	mux := http.NewServeMux()
@@ -16,5 +19,8 @@ func (app *application) routes() http.Handler{
 	mux.HandleFunc("GET /snippet/create",app.snippetCreate)
 	mux.HandleFunc("POST /snippet/create",app.snippetCreatePost)
 
-	return app.recoverPanic(app.logRequest(commonHeaders(mux)))
+	standard := alice.New(app.recoverPanic,app.logRequest,commonHeaders)
+
+	return standard.Then(mux)
+
 }
