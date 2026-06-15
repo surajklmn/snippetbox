dc16d2e174e97c9364e3f731af1ab58b18648d35
surajklmn <surajbhattarai339@gmail.com>
2026-06-15 17:07:05 +0000

    validate form data such as title, content and expires value

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"strconv"
 	"errors"
+	"strings"
+	"unicode/utf8"
 
 	"github.com/surajklmn/snippetbox/internal/models"
 )
@@ -120,6 +122,30 @@ func (app *application)snippetCreatePost(w http.ResponseWriter,r *http.Request){
 		app.serverError(w,r,err)
 		return
 	}
+
+
+	fieldErrors := make(map[string] string)
+	//Check the title isnot blank and isnot more than 100 charcaters
+	if strings.TrimSpace(title) == ""{
+		fieldErrors["title"] = "This field cannot be blank"
+	}else if utf8.RuneCountInString(title) > 100{
+		fieldErrors["title"] = "This field cannot be more than 100 characters long"
+	}
+	
+	// Check content value isnot empty
+	if strings.TrimSpace(content) == ""{
+		fieldErrors["content"] = "This field cannot be blank"
+	}
+	// Check expires value 
+	if expires != 1 && expires != 7 && expires != 365{
+		fieldErrors["expires"] = "This field must equal 1,7 or 365"
+	}
+
+	if len(fieldErrors) > 0 {
+		fmt.Fprint(w,fieldErrors)
+		return
+	}
+
 	id, err := app.snippets.Insert(title, content, expires)
 	if err!= nil{
 		app.serverError(w,r,err)
