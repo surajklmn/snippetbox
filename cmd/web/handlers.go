8234c55efc8b96c440bbb0d5d3aad3feec7ac3fe
surajklmn <surajbhattarai339@gmail.com>
2026-06-15 16:47:52 +0000

    Parse form data

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -107,15 +107,24 @@ func (app *application)snippetCreate(w http.ResponseWriter, r *http.Request){
 }
 
 func (app *application)snippetCreatePost(w http.ResponseWriter,r *http.Request){
-	title := "O snail"
-	content := "O snail Climb Moun Fuji,jkdfjkajdkj"
-	expires := 7
-	id, err := app.snippets.Insert(title, content, expires)
+
+	err := r.ParseForm()
+	if err!= nil{
+		app.clientError(w,http.StatusBadRequest)
+		return
+	}
+	title := r.PostForm.Get("title")
+	content := r.PostForm.Get("content")
+	expires,err := strconv.Atoi(r.PostForm.Get("expires"))
 	if err != nil{
 		app.serverError(w,r,err)
 		return
 	}
-
+	id, err := app.snippets.Insert(title, content, expires)
+	if err!= nil{
+		app.serverError(w,r,err)
+		return
+	}
 	http.Redirect(w,r,fmt.Sprintf("/snippet/view/%d",id),http.StatusSeeOther)
 
 
